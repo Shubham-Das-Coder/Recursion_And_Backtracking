8adfe8cad4593c40e57ad3b4a8431e076f4d6f3a
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2021-11-17 13:29:39 +0530

    Add files via upload

---
+++ Tower_Of_Hanoi_Using_Recursion.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Tower_Of_Hanoi_Using_Recursion {
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int t1d = scn.nextInt();
+        int t2d = scn.nextInt();
+        int t3d = scn.nextInt();
+        toh(n, t1d, t2d, t3d);
+    }
+
+    public static void toh(int n, int t1id, int t2id, int t3id) {
+        if (n == 0) {
+            return;
+        }
+        toh(n - 1, t1id, t3id, t2id); // Will print the instructions to move n-1 discs from t1 to t3 using t2
+        System.out.println(n + "[" + t1id + " -> " + t2id + "]");
+        toh(n - 1, t3id, t2id, t1id); //
+    }
+}
